ce73941abfe9b07fb1167a27fc3b252ce632b3b1
TijlIvens <tijl.ivens@gmail.com>
2019-11-28 12:51:58 +0100

    styling

--- src/components/NavMenuComponent/NavMenuItem/NavMenuItem.jsx
+++ src/components/NavMenuComponent/NavMenuItem/NavMenuItem.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import color from "@material-ui/core/colors/amber";
 
 const NavMenuItem = props => {
+  const color = {
+    color: "#eeeeee"
+  };
   return (
     <div>
       <ListItem button onClick={props.clickOnTitle.bind(this, props.item.name)}>
-        <ListItemText primary={props.item.name} />
+        <p style={color}>{props.item.name}</p>
+        <ListItemText />
       </ListItem>
     </div>
   );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import NavMenuComponent from "../../components/NavMenuComponent/NavMenuComponent";
 import VisualiseComponent from "../../components/VisualiseComponent/VisualiseComponent";
+import "./Dachboard.css";
 
 const Dashboard = props => {
-    const apiBaseUrl = "https://htf.zinderlabs.com";
-    const apiKey = "c5845908142885ccbb0123093471c2b6";
-    const [currentBank, setCurrentBank] = useState(null);
+  const apiBaseUrl = "https://htf.zinderlabs.com";
+  const apiKey = "c5845908142885ccbb0123093471c2b6";
+  const [currentBank, setCurrentBank] = useState(null);
 
-    return (
-        <div>
-            <NavMenuComponent
-                apiBaseUrl={apiBaseUrl}
-                apiKey={apiKey}
-                setCurrentBank={setCurrentBank}
-            />
-            <VisualiseComponent apiBaseUrl={apiBaseUrl} apiKey={apiKey} />
-        </div>
-    );
-}
-
+  return (
+    <div className="FlexBox">
+      <NavMenuComponent
+        className="NavMenu"
+        apiBaseUrl={apiBaseUrl}
+        apiKey={apiKey}
+        setCurrentBank={setCurrentBank}
+      />
+      <VisualiseComponent apiBaseUrl={apiBaseUrl} apiKey={apiKey} />
+    </div>
+  );
+};
 
 export default Dashboard;
