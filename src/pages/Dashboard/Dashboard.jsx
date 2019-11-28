03cce8c10643273c1984dd78fe430a8315855cbc
bmike7 <bijl.mike@outlook.be>
2019-11-28 11:35:40 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import NavMenuComponent from "../../components/NavMenuComponent/NavMenuComponent
 import VisualiseComponent from "../../components/VisualiseComponent/VisualiseComponent";
 
 const Dashboard = props => {
+<<<<<<< Updated upstream
   const apiBaseUrl = "https://htf.zinderlabs.com";
   const apiKey = "c5845908142885ccbb0123093471c2b6";
   const [currentBank, setCurrentBank] = useState(null);
@@ -17,6 +18,17 @@ const Dashboard = props => {
     </div>
   );
       <VisualiseComponent apiBaseUrl={apiBaseUrl} apiKey={apiKey} />
+=======
+    const apiBaseUrl = "https://htf.zinderlabs.com";
+    const apiKey = "c5845908142885ccbb0123093471c2b6";
+
+    return (
+        <div>
+            <NavMenuComponent />
+            <VisualiseComponent apiBaseUrl={apiBaseUrl} apiKey={apiKey} />
+        </div>
+    );
+>>>>>>> Stashed changes
 };
 
 export default Dashboard;
