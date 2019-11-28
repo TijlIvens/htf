c921e4061f309573925cc6338140c87f435cc359
bmike7 <bijl.mike@outlook.be>
2019-11-28 10:41:39 +0100

    use of material ui

--- src/components/AccountComponent/AccountComponent.jsx
+++ src/components/AccountComponent/AccountComponent.jsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import "./AccountComponent.css"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const AccountCard = props => {
-    return (
-        <div className="accountCard">
-            <div className="name">{`${props.account.firstName} ${props.account.lastName}`}</div>
+    const classes = useStyles()
 
+    return (
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography className={classes.title}>{`${props.account.firstName} ${props.account.lastName}`}</Typography>
+                <Typography className={classes.content}>{`Balance: ${props.account.account.balance} ${props.account.account.currency}`}</Typography>
+            </CardContent>
             {console.log(props.account)}
-        </div>
+        </Card>
     );
 }
 
@@ -37,3 +46,19 @@ const AccountComponent = props => {
 }
 
 export default AccountComponent;
+
+
+const useStyles = makeStyles({
+    card: {
+        minWidth: "25%",
+        display: "inline-block",
+        margin: "10px"
+    },
+    title: {
+        fontSize: '14px',
+        marginBottom: '10px'
+    },
+    content: {
+        fontSize: '10px',
+    }
+})
